4468c8b024e1698fae5a240e1a64ca1edf78d7d3
sihyuk <sihyukmhrjn@gmail.com>
2022-05-19 18:01:45 +0545

    Change Title

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,10 +4,10 @@ export const getGlobalData = () => {
     : 'Bibek Maharjan';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Portfolio';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : '@Copyright';
 
   return {
     name,
